a92d47e6c47c845292c7076592575cebfb597f1d
Leonid Tšigrinski <leonid.tsigrinski@defsecintel.com>
2026-08-10 22:41:12 +0300

    Unit test

--- test_app.py
+++ test_app.py
@@ -7,4 +7,4 @@ def test_hello_world():
     response = client.get("/")
 
     assert response.status_code == 200
-    assert response.text == "Hello, world!\n"
+    assert response.text == "Hello from CI/CD!\n"
